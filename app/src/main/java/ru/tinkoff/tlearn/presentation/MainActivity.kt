de2be545f65fdcc3bea50208b4b9fac4698d3e87
Naloaty <53543446+Naloaty@users.noreply.github.com>
2022-10-31 15:38:22 +0500

    Project: Create initial package structure

--- app/src/main/java/ru/tinkoff/tlearn/presentation/MainActivity.kt
+++ app/src/main/java/ru/tinkoff/tlearn/presentation/MainActivity.kt
@@ -1,7 +1,8 @@
-package ru.tinkoff.tlearn
+package ru.tinkoff.tlearn.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import ru.tinkoff.tlearn.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
